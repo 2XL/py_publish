35e90d687871c9d928ca41dff69f1a1b6da1f1ef
chenglong zq <chenglong.zq@gmail.com>
2016-06-23 18:43:05 +0200

    Halt publisher_credentials editions refresh

--- plugin/dropbox.py
+++ plugin/dropbox.py
@@ -1,4 +1,6 @@
 import dropbox
+import os
+import sys
 from publisher_credentials import CREDENTIALS_DROPBOX
 
 
@@ -7,15 +9,32 @@ class Dropbox():
     def __init__(self):
 
         self.whoami = (self).__class__.__name__
+        self.client
         print self.whoami
 
     def hello(self):
         print "{} say hello".format(self.whoami)
-        client = dropbox.client.DropboxClient()
-        print 'linked account: ', client.account_info()
-        f = open('sample/sample.txt','rb')
-        response = client.put_file('/sample.txt', f)
-        print 'uploaded: ', response
+
+        """
+        Connect and authenticate with dropbox
+        """
+
+        app_key = CREDENTIALS_DROPBOX['app_key']
+        app_secret = CREDENTIALS_DROPBOX['app_secret']
+        access_type = "dropbox"
+        session = dropbox.session.DropboxSession(app_key, app_secret, access_type)
+
+        request_token = session.obtained_request_token()
+
+        url = session.build_authorize_url(request_token)
+        msg
+
+
+        #
+        # print 'linked account: ', client.account_info()
+        # f = open('sample/sample.txt','rb')
+        # response = client.put_file('/sample.txt', f)
+        # print 'uploaded: ', response
 
     def publish(self, src, tgt):
         print "{} say publish".format(self.whoami)
--- publisher_credentials.py
+++ publisher_credentials.py
@@ -1,11 +1,11 @@
 
 # credentials:
 
+# Add OAuth2 access token here.
+# You can generate one for yourself in the App Console.
+# See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
 CREDENTIALS_DROPBOX = {
-
-"auth_token" : "TOKEN HERE",
-
-
+"auth_token" : "MphEvDU0fIAAAAAAAAAEPA2aa964aABpgj4g_GI3jFD_5XyoqzMQdjTt2m4j9Cbs",
 }
 
 CREDENTIALS_MEGA = {
